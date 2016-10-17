4bcc7cd9562ad8b81dbe1f228c4f5cc830f756ff
ZacSmir <Zach@smirin.us>
2016-10-17 14:48:28 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-document.getElementById('money').innerHTML += 'you have';
+var moneys = 0
+document.getElementById('money').innerHTML += 'you have ' + moneys + " million dollars";
 document.getElementById("click").onclick = function click() {
   console.log("stuff")
+  var money = money + 
 }
