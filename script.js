c5cd0d15dccc30eb9ea53eceebba0d745c523fc0
ZacSmir <Zach@smirin.us>
2016-10-17 15:05:58 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+var listvalue = "trumpclicker"
+
 var clickmoney = 1
 var money = 0
 document.getElementById('money').innerHTML = 'you have ' + money + " million dollars";
 document.getElementById("click").onclick = function click() {
   money += clickmoney;
   document.getElementById('money').innerHTML = 'you have ' + money + " million dollars";
+  addListItem(listvalue, valueToSave);
 }
