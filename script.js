50b1ce275acec13d575c7f6a26b77d4321acfbb0
ZacSmir <Zach@smirin.us>
2016-10-24 09:35:50 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ document.getElementById("click").onclick = function click() {
   var money2 = moneyowed+1
   moneyowed = money2*1.01
   console.log(moneyowed)
+  var moneyowed-val
   debt.html("you also have " + moneyowed + " million dollars of debt")
   
   
