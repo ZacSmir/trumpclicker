dbccc4a598a718c38be494370978b9990c86bbbd
ZacSmir <Zach@smirin.us>
2016-11-02 09:51:44 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //var listvalue = ["trumpclicker"]
 var debt = $("#debt")
 var clickmoney = 1
-var money = Number(localStorage.loadmoney)
+var money = Number(localStorage.loadmoney || 0)
 var moneyowedvalue = 0
-var moneyowed = Number(localStorage.loaddebt)
+var moneyowed = Number(localStorage.loaddebt || 0)
 //document.getElementById('money').innerHTML = 'you have ' + money + " million dollars";
 
 document.getElementById("click").onclick = function click() {
